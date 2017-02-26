48f61a182d79cea58e82074735a4873e1d9209ea
PuQian <puqian1992@163.com>
2017-02-26 16:34:17 +0800

    summary

---
+++ README.md
@@ -0,0 +1,3 @@
+# 基于 Vue.js 实现的外卖手机页面
+
+项目介绍：[http://matuan.coding.me]()
---
+++ prod.server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var config = require('./config/index');
+
+var port = process.env.PORT || config.build.port;
+
+var app = express();
+
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+  req.url = './index.html';
+  next();
+});
+
+app.use(router);
+
+var appData = require('./data.json')
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+var apiRoutes = express.Router();
+
+// 模拟服务器端返回数据
+apiRoutes.get('/seller', function(req, res) {
+  res.json({
+    errno: 0,
+    data: seller 
+  });
+});
+
+apiRoutes.get('/goods', function(req, res) {
+  res.json({
+    errno: 0,
+    data: goods
+  });
+});
+
+apiRoutes.get('/ratings', function(req, res) {
+  res.json({
+    errno: 0,
+    data: ratings
+  });
+});
+
+// api/goods取到数据
+app.use('/api', apiRoutes);
+
+app.use(express.static('./dist'));
+
+// 启动 http server
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:' + port + '\n')
+})
+
+
+
+
+
